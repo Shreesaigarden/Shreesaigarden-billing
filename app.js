1d0cbeb52e42a6752f31654be14340a0a584fbfd
Shreesaigarden <behedeanish13@gmail.com>
2026-08-04 14:58:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const billingArea = document.getElementById("billingArea");
 const newBill = document.getElementById("newBill");
-
 const tbody = document.querySelector("#billTable tbody");
-
 const grandTotal = document.getElementById("grandTotal");
 
 let bill = {};
@@ -12,62 +10,62 @@ newBill.onclick = function () {
     billingArea.style.display = "block";
 };
 
+
 function addItem(name, price) {
 
     if (!bill[name]) {
-
         bill[name] = {
             qty: 0,
             price: price
         };
-
     }
 
     bill[name].qty++;
 
     renderBill();
-
 }
+
 
 function renderBill() {
 
     tbody.innerHTML = "";
-
     total = 0;
 
     for (let item in bill) {
 
         let qty = bill[item].qty;
-
         let price = bill[item].price;
-
         let itemTotal = qty * price;
 
         total += itemTotal;
 
         tbody.innerHTML += `
-<tr>
-<td>${item}</td>
-<td>${qty}</td>
-<td>₹${price}</td>
-<td>₹${itemTotal}</td>
-</tr>
-`;
-
+        <tr>
+            <td>${item}</td>
+            <td>${qty}</td>
+            <td>₹${price}</td>
+            <td>₹${itemTotal}</td>
+        </tr>`;
     }
 
     grandTotal.innerHTML = "Grand Total : ₹" + total;
-
 }
 
-document.getElementById("paneer").onclick = () => addItem("Paneer Masala",180);
 
-document.getElementById("roti").onclick = () => addItem("Butter Roti",25);
+// Buttons
+document.getElementById("paneer").addEventListener("click", function(){
+    addItem("Paneer Masala",180);
+});
 
-document.getElementById("tea").onclick = () => addItem("Tea",20);
+document.getElementById("roti").addEventListener("click", function(){
+    addItem("Butter Roti",25);
+});
+
+document.getElementById("tea").addEventListener("click", function(){
+    addItem("Tea",20);
+});
+    
 
 document.getElementById("saveBill").onclick = function(){
-
-alert("Bill Saved Successfully ✅");
-
+    alert("Bill Saved Successfully ✅");
 };
