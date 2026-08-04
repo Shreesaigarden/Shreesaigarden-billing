d8aa24cb8fc9fc6128b4ba6269eec8bd0491f34e
Shreesaigarden <behedeanish13@gmail.com>
2026-08-04 14:45:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 let total = 0;
 
 const billingArea = document.getElementById("billingArea");
+const newBill = document.getElementById("newBill");
+const totalText = document.getElementById("total");
 
-document.getElementById("newBill").onclick = function () {
+newBill.addEventListener("click", function () {
     billingArea.style.display = "block";
-};
+});
 
-document.getElementById("paneer").onclick = function () {
+document.getElementById("paneer").addEventListener("click", function () {
     total += 180;
-    document.getElementById("total").innerHTML = "Total ₹" + total;
-};
+    totalText.textContent = "Total ₹" + total;
+});
 
-document.getElementById("roti").onclick = function () {
+document.getElementById("roti").addEventListener("click", function () {
     total += 25;
-    document.getElementById("total").innerHTML = "Total ₹" + total;
-};
+    totalText.textContent = "Total ₹" + total;
+});
 
-document.getElementById("tea").onclick = function () {
+document.getElementById("tea").addEventListener("click", function () {
     total += 20;
-    document.getElementById("total").innerHTML = "Total ₹" + total;
-};
+    totalText.textContent = "Total ₹" + total;
+});
