73018ad2d1eff51075612bf0f7b7648c34ab6319
Shreesaigarden <behedeanish13@gmail.com>
2026-08-04 14:41:27 +0530

    Rename billArea and improve event handling
    Refactor billArea variable name and update event listener logic.

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-const billArea = document.getElementById("billingArea");
+const billingArea = document.getElementById("billingArea");
 const newBill = document.getElementById("newBill");
 
-let total = 0;
-
-newBill.addEventListener("click", () => {
-    billArea.style.display = "block";
-});
-
-const buttons = document.querySelectorAll("#billingArea button");
-
-buttons.forEach(btn => {
-
-    btn.addEventListener("click", () => {
-
-        if(btn.innerText.includes("Paneer")){
-            total += 180;
-        }
-
-        if(btn.innerText.includes("Butter")){
-            total += 25;
-        }
-
-        if(btn.innerText.includes("Tea")){
-            total += 20;
-        }
-
-        document.querySelector("#billingArea h3:last-child").innerHTML =
-        "Total : ₹" + total;
-
+if (newBill && billingArea) {
+    newBill.addEventListener("click", () => {
+        billingArea.style.display = "block";
     });
-
-});
+}
