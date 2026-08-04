c64b426b9bd91533f96e61c35b72481d27063dd8
Shreesaigarden <behedeanish13@gmail.com>
2026-08-04 14:51:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,73 @@
-let total = 0;
-
 const billingArea = document.getElementById("billingArea");
 const newBill = document.getElementById("newBill");
-const totalText = document.getElementById("total");
 
-newBill.addEventListener("click", function () {
-    billingArea.style.display = "block";
-});
+const tbody = document.querySelector("#billTable tbody");
 
-document.getElementById("paneer").addEventListener("click", function () {
-    total += 180;
-    totalText.textContent = "Total ₹" + total;
-});
+const grandTotal = document.getElementById("grandTotal");
 
-document.getElementById("roti").addEventListener("click", function () {
-    total += 25;
-    totalText.textContent = "Total ₹" + total;
-});
-
-document.getElementById("tea").addEventListener("click", function () {
-    total += 20;
-    totalText.textContent = "Total ₹" + total;
-});
+let bill = {};
 let total = 0;
-
-const billingArea = document.getElementById("billingArea");
-const newBill = document.getElementById("newBill");
-const totalText = document.getElementById("total");
-const billList = document.getElementById("billList");
 
 newBill.onclick = function () {
     billingArea.style.display = "block";
 };
 
 function addItem(name, price) {
-    total += price;
 
-    const li = document.createElement("li");
-    li.textContent = `${name} - ₹${price}`;
-    billList.appendChild(li);
+    if (!bill[name]) {
 
-    totalText.textContent = "Total ₹" + total;
+        bill[name] = {
+            qty: 0,
+            price: price
+        };
+
+    }
+
+    bill[name].qty++;
+
+    renderBill();
+
 }
 
-document.getElementById("paneer").onclick = () => addItem("Paneer Masala", 180);
-document.getElementById("roti").onclick = () => addItem("Butter Roti", 25);
-document.getElementById("tea").onclick = () => addItem("Tea", 20);
+function renderBill() {
+
+    tbody.innerHTML = "";
+
+    total = 0;
+
+    for (let item in bill) {
+
+        let qty = bill[item].qty;
+
+        let price = bill[item].price;
+
+        let itemTotal = qty * price;
+
+        total += itemTotal;
+
+        tbody.innerHTML += `
+<tr>
+<td>${item}</td>
+<td>${qty}</td>
+<td>₹${price}</td>
+<td>₹${itemTotal}</td>
+</tr>
+`;
+
+    }
+
+    grandTotal.innerHTML = "Grand Total : ₹" + total;
+
+}
+
+document.getElementById("paneer").onclick = () => addItem("Paneer Masala",180);
+
+document.getElementById("roti").onclick = () => addItem("Butter Roti",25);
+
+document.getElementById("tea").onclick = () => addItem("Tea",20);
+
+document.getElementById("saveBill").onclick = function(){
+
+alert("Bill Saved Successfully ✅");
+
+};
